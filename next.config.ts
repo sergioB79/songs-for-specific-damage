9810402cda6f47452288095abec315b231ce5720
sergioB79 <sergiobatalha79@gmail.com>
2026-09-10 04:45:20 +0100

    fix: disable Next image optimizer for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   basePath: repoBase,
   assetPrefix: repoBase || undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
